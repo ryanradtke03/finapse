8ae78bcebbba89ec1c15f067ee0f0818e31c9c30
Ryan Radtke <95334241+ryanradtke03@users.noreply.github.com>
2026-04-02 20:01:38 -0700

    Finished basic Budget CRUD (#29)
    * Finished basic Budget CRUD
    
    * updated unused var
    
    * Fixed useEffect on AuthModal
    
    * Updated AuthModal

--- apps/api/src/routes.ts
+++ apps/api/src/routes.ts
@@ -1,12 +1,16 @@
 import { Router } from 'express'
 import authRoutes from './features/auth/auth.route'
+import budgetRouter from './features/budget/budget.route'
 import healthRouter from './features/health/health.route'
 import plaidRouter from './features/plaid/plaid.route'
+
 
 const router = Router()
 
 router.use('/auth', authRoutes)
 router.use('/plaid', plaidRouter)
+router.use('/budget', budgetRouter);
 router.use('/health', healthRouter)
 
-export default router
+
+export default router;
